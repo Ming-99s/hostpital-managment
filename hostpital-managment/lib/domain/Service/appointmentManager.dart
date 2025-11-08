0ff5d0103a86789bf7e002f6bcb3701495e79713
Ming-99s <lyming4999@gmail.com>
2025-11-08 22:01:08 +0700

    add userManager

--- hostpital-managment/lib/domain/Service/appointmentManager.dart
+++ hostpital-managment/lib/domain/Service/appointmentManager.dart
@@ -1,29 +1,9 @@
-import 'package:uuid/uuid.dart';
-import '../appointment.dart';
-import '../user.dart';
-import '../doctor.dart';
+import '../../data/Repository/appointments_file.dart';
 
 class AppointmentManager {
-  late List<User> _users;
-  late List<Appointment> _appointments;
+  final AppointmentRepository appointmentRepo;
 
-  AppointmentManager({
-    required List<User> users,
-    required List<Appointment> appointments,
-  }) { 
-    _users = users;
-    _appointments = appointments;
-  }
-
-  // -------------------- Getters --------------------
-  List<User> get allUsers => _users;
-  List<Appointment> get allAppointments => _appointments;
-
-  List<User> get allDoctors =>
-      _users.where((u) => u.type == UserType.doctor).toList();
-
-  List<User> get allPatients =>
-      _users.where((u) => u.type == UserType.patient).toList();
+  AppointmentManager(this.appointmentRepo);
 
 
 }
---
+++ hostpital-managment/lib/domain/Service/userManager.dart
@@ -0,0 +1,7 @@
+import '../../data/Repository/User_file.dart';
+
+class UserManager {
+  final UserRepository userRepository;
+
+  UserManager({required this.userRepository});
+}
